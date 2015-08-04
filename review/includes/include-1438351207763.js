00fde1b3c1fd91d5addc2dd26dbf2c85333f93c1
Sam Wang <kitlenv@gmail.com>
2015-08-04 14:40:01 +1000

    make it work

--- review/includes/include-1438351207763.js
+++ review/includes/include-1438351207763.js
@@ -1,4 +1,4 @@
-document.write("<script type='text/javascript' charset='utf-8' src='file:////Applications/Justinmind.app/Contents/Resources/Java/resources/_jim/javascript/jim-min.js'></script>");
-document.write("<script type='text/javascript' charset='utf-8' src='file:////Applications/Justinmind.app/Contents/Resources/Java/resources/_jim/javascript/mobile/ios/ios8/iphone/jim-iphone-min.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='../../resources/_jim/javascript/jim-min.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='../../resources/_jim/javascript/mobile/ios/ios8/iphone/jim-iphone-min.js'></script>");
 document.write("<script type='text/javascript' charset='utf-8' src='./resources/scroll-1438351207763.js'></script>");
 document.write("<script type='text/javascript' charset='utf-8' src='./resources/lookup-1438351207763.js'></script>");
